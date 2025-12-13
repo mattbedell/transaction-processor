e6f6eb35a5a8b198ade275df4fac8ef3506df37c
Matt Bedell <matt@bedell.co>
2025-12-13 13:11:11 -0500

    Impl new for account, rm total field

--- src/account.rs
+++ src/account.rs
@@ -1,12 +1,23 @@
+#[derive(Default, Clone, Copy)]
 pub enum AccountStatus {
+    #[default]
     Active,
     Locked,
 }
 
+#[derive(Default)]
 pub struct Account {
     id: u16,
     available: f64,
     held: f64,
-    total: f64,
     status: AccountStatus
+}
+
+impl Account {
+    pub fn new(id: u16) -> Self {
+        Account {
+            id,
+            ..Account::default()
+        }
+    }
 }
