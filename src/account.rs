37db30640e00130e10ff19766a7f4c73c982e550
Matt Bedell <matt@bedell.co>
2025-12-13 13:39:51 -0500

    Add account error, funds methods

--- src/account.rs
+++ src/account.rs
@@ -1,3 +1,5 @@
+use crate::error::{AccountError, TransactionError};
+
 #[derive(Default, Clone, Copy)]
 pub enum AccountStatus {
     #[default]
@@ -10,7 +12,7 @@ pub struct Account {
     id: u16,
     available: f64,
     held: f64,
-    status: AccountStatus
+    status: AccountStatus,
 }
 
 impl Account {
@@ -19,5 +21,38 @@ impl Account {
             id,
             ..Account::default()
         }
+    }
+
+    pub fn hold(&mut self, amount: f64) -> Result<(), AccountError> {
+        if 0.0 < self.available - amount {
+            self.available -= amount;
+            self.held += amount;
+            Ok(())
+        } else {
+            Err(AccountError::InsufficientFunds { id: self.id })
+        }
+    }
+
+    // @todo: duped and dirty, clean this up
+    pub fn free(&mut self, amount: f64) -> Result<(), AccountError> {
+        if 0.0 < self.held - amount {
+            self.held -= amount;
+            self.available += amount;
+            Ok(())
+        } else {
+            Err(AccountError::InsufficientHold { id: self.id })
+        }
+    }
+
+    fn set_status(&mut self, status: AccountStatus) {
+        self.status = status;
+    }
+
+    pub fn lock(&mut self) {
+        self.set_status(AccountStatus::Locked)
+    }
+
+    pub fn set_active(&mut self) {
+        self.set_status(AccountStatus::Active)
     }
 }
--- src/error.rs
+++ src/error.rs
@@ -2,14 +2,48 @@ use std::error::Error;
 use std::fmt::Display;
 
 #[derive(Debug)]
+pub enum AccountError {
+    InsufficientFunds { id: u16 },
+}
+
+impl Error for AccountError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            AccountError::InsufficientFunds { .. } => None,
+        }
+    }
+}
+
+impl Display for AccountError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AccountError::InsufficientFunds { id } => {
+                write!(f, "InsufficientFunds in account: '{id}'")
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum TransactionError {
-    InsufficientFunds { account: u16, tx: u32 },
+    InsufficientFunds { tx: u32, source: AccountError },
+}
+
+impl TransactionError {
+    pub fn from_account_error(err: AccountError, transaction_id: u32) -> Self {
+        match err {
+            AccountError::InsufficientFunds { .. } => TransactionError::InsufficientFunds {
+                tx: transaction_id,
+                source: err,
+            },
+        }
+    }
 }
 
 impl Error for TransactionError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            TransactionError::InsufficientFunds { .. } => None,
+            TransactionError::InsufficientFunds { source, .. } => Some(source),
         }
     }
 }
@@ -17,8 +51,8 @@ impl Error for TransactionError {
 impl Display for TransactionError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            TransactionError::InsufficientFunds { account, tx } => {
-                write!(f, "InsufficientFunds: account: {account}, tx: {tx}")
+            TransactionError::InsufficientFunds { tx, .. } => {
+                write!(f, "InsufficientFunds tx: '{tx}'")
             }
         }
     }
