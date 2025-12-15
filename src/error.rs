fa52c1559e4f5172b8e3d8c9c51ab5dd47ffe1be
Matt Bedell <matt@bedell.co>
2025-12-15 05:50:50 -0500

    Refactor errors

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,23 @@
 use std::error::Error;
 use std::fmt::Display;
 
+use crate::transaction::TransactionEventType;
+
 #[derive(Debug)]
 pub enum AccountError {
-    InsufficientFunds { id: u16 },
+    InsufficientFunds { id: u16, tx: u32 },
+    InsufficientHold { id: u16, tx: u32 },
+    ExpectedAmountError { id: u16, tx: u32 },
+    AccountFrozen { id: u16, tx: u32 },
 }
 
 impl Error for AccountError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
             AccountError::InsufficientFunds { .. } => None,
+            AccountError::InsufficientHold { .. } => None,
+            AccountError::ExpectedAmountError { .. } => None,
+            AccountError::AccountFrozen { .. } => None,
         }
     }
 }
@@ -17,43 +25,51 @@ impl Error for AccountError {
 impl Display for AccountError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            AccountError::InsufficientFunds { id } => {
-                write!(f, "InsufficientFunds in account: '{id}'")
+            AccountError::InsufficientFunds { id, tx } => {
+                write!(f, "InsufficientFunds in account: '{id}', tx: '{tx}'")
+            }
+            AccountError::InsufficientHold { id, tx } => {
+                write!(f, "InsufficientHold in account: '{id}', tx: '{tx}'")
+            }
+            AccountError::ExpectedAmountError { id, tx } => {
+                write!(
+                    f,
+                    "ExpectedAmountError: '{id}', tx: '{tx}', expected tx amount"
+                )
+            }
+            AccountError::AccountFrozen { id, tx } => {
+                write!(
+                    f,
+                    "AccountFrozen: '{id}', tx: '{tx}', expected tx amount"
+                )
             }
         }
     }
 }
 
 #[derive(Debug)]
-pub enum TransactionError {
-    InsufficientFunds { tx: u32, source: AccountError },
+pub enum TxEventError {
+    UnexpectedTxType {
+        actual: TransactionEventType,
+        expected: TransactionEventType,
+    },
 }
 
-impl TransactionError {
-    pub fn from_account_error(err: AccountError, transaction_id: u32) -> Self {
-        match err {
-            AccountError::InsufficientFunds { .. } => TransactionError::InsufficientFunds {
-                tx: transaction_id,
-                source: err,
-            },
-        }
-    }
-}
-
-impl Error for TransactionError {
+impl Error for TxEventError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            TransactionError::InsufficientFunds { source, .. } => Some(source),
+            TxEventError::UnexpectedTxType { .. } => None,
         }
     }
 }
 
-impl Display for TransactionError {
+impl Display for TxEventError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            TransactionError::InsufficientFunds { tx, .. } => {
-                write!(f, "InsufficientFunds tx: '{tx}'")
-            }
+            TxEventError::UnexpectedTxType { actual, expected } => write!(
+                f,
+                "UnexpectedTxType: expected: '{expected:?}', actual: '{actual:?}'"
+            ),
         }
     }
 }
